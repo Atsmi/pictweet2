4fdd321371cf6f6f148d240300c7d491592d3085
LAPTOP-E0D99GDE\mya-m <sea.of.greentea.a@gmail.com>
2016-10-23 15:54:23 +0900

    ルーティングをネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  resources :tweets                     #tweets_controllerに対してのresourcesメソッド
-  resources :users, only: [:show]       #users_controllerに対してのresourcesメソッド
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
 end
